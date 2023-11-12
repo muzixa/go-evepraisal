29991729de08cd4c3101a26f705911924c9caf9f
Andy <muzixa@gmail.com>
2023-11-12 22:43:02 +0300

    Update templates.go

--- web/templates.go
+++ web/templates.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/dustin/go-humanize"
-	"github.com/evepraisal/go-evepraisal"
+	"github.com/muzixa/go-evepraisal"
 	"github.com/pquerna/ffjson/ffjson"
 )
 
