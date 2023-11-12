98795e30f56cca9cd02983e68fef86833b5599ca
Andy <muzixa@gmail.com>
2023-11-12 22:29:12 +0300

    Update restore.go

--- legacy/restore.go
+++ legacy/restore.go
@@ -11,8 +11,8 @@ import (
 	"strings"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/evepraisal/go-evepraisal"
-	"github.com/evepraisal/go-evepraisal/typedb"
+	"github.com/muzixa/go-evepraisal"
+	"github.com/muzixa/go-evepraisal/typedb"
 )
 
 // RestoreLegacyFile will load a given restore file into the database
